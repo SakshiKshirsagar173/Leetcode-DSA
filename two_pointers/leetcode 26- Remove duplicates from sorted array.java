4ca964c15cb965a570cc6b7ec41b241a34cfad6b
Sakshi Kshirsagar <sakshiik20@gmail.com>
2026-04-06 14:04:06 +0530

    Update leetcode 26- Remove duplicates from sorted array.java

--- two_pointers/leetcode 26- Remove duplicates from sorted array.java
+++ two_pointers/leetcode 26- Remove duplicates from sorted array.java
@@ -1,13 +1,18 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        if not nums:
-            return 0
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
 
-        i = 0
+        int i = 0;
 
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:
-                i += 1
-                nums[i] = nums[j]
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
 
-        return i + 1
+        return i + 1;
+    }
+}
